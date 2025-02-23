a70ec5e310d6071438a1feff6a7a3a36b0c5c39c
Deaquicomocoqui <jalifedbrenvazquez@gmail.com>
2025-02-23 00:42:35 -0400

    Update rollercoaster.py

--- rollercoaster.py
+++ rollercoaster.py
@@ -1,29 +1,49 @@
+art = r''' 
+Welcome                                       __
+                                       _  |  |
+                   Yye                |_| |--|
+                .---.  e           AA | | |  |
+               /.--./\  e        A
+              // || \/\  e      A
+             //|/|| |\/\   aa a    |\o/ o/--
+            ///|\|| | \/\ .       ~o \.'\.o'
+           //|\|/|| | |\/\ .      /.` \o'
+          //\|/|\|| | | \/\ ( (  . \o'
+  __ __ _//|/|\|/|| | | |\/`--' '
+ __/__/__//|\|/|\|| | | | `--'
+ |\|/|\|/|\|/|\|/|| | | | |
+RG'''
 
-print("""
-      ,--.--._
-------" _, \___)
-        / _/____)
-        \//(____)
-------\     (__)
-       `-----
-       """)
-
+print(art)
 print("\nWelcome to the rollercoaster!")
+
+# Get height input from the user
 height = int(input("What is your height in Centimeters (cm)? \n"))
+
 if height >= 120:
     print("\nYou can ride the rollercoaster safely!")
+    
+    # Get age input from the user
     age = int(input("What is your age? "))
+    
+    # Determine the base price based on age
     if age <= 12:
         bill = 5
     elif age <= 18:
         bill = 7
-    elif age >= 45 and age <= 55:
+    elif 45 <= age <= 55:
         print("Everything is going to be ok. Have a free ride on us!")
+        bill = 0  # Free ride for 45-55 age group
     else:
         bill = 12
-    want_photo = input("Do you want to have a photo taken? Type y for Yes and n for No.\n")
+
+    # Ask if the user wants a photo
+    want_photo = input("Do you want to have a photo taken? Type y for Yes and n for No.\n").lower()
+    
     if want_photo == "y":
-        bill = bill + 3
-    print(f'Your final bill is {bill}')
+        bill += 3
+    
+    # Print the final bill
+    print(f'Your final bill is ${bill}')
 else:
-    print("\nSorry your height is not currently allowed in this ride, its for your own safety")
+    print("\nSorry, your height is not currently allowed on this ride, it's for your own safety.")
